4c3c77fb6d1735b1389cfb08f49038bfee12a1e2
1050246180 <1050246180@qq.com>
2021-01-26 14:15:29 +0800

    no message

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
-    
+
+    public String login(){
+        return "登录成功";
+    }
+
+    public void logout(){
+        String str =  "注销成功";
+    }
 }
