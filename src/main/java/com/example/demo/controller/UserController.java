00463ad491518ebffa5610486ac805615f3a5573
1050246180 <1050246180@qq.com>
2022-07-05 11:23:41 +0800

    add：添加模拟数据接口16

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -12,7 +12,7 @@ public class UserController {
      */
     @RequestMapping("/data")
     public String data(){
-        System.out.println("15===================data==============");
+        System.out.println("16===================data==============");
         return "hello word";
     }
     public String login(){
